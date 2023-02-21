be515434e266848efa183dd213ebf1129ea9db4f
MoizADev <114066668+MoizADev@users.noreply.github.com>
2023-02-21 19:55:43 +0500

    branch deletion and get branch by id added

--- backend/controllers/branch.js
+++ backend/controllers/branch.js
@@ -16,6 +16,27 @@ export const createBranch = (req, res, next) => {
     })
 }
 
+export const getBranchById = (req, res, next) => {
+    BranchSchema.findById(req.params.id)
+    .then((response)=>{
+        if (!response) {
+            throw ("Branch Not Found");
+        }
+        else {
+            res.status(200).json({
+                success: true,
+                branch: response
+            })
+        }
+    })
+    .catch((error) => {
+        res.status(401).json({
+            success: false,
+            error: error
+        })
+    })
+}
+
 export const loadBranchCollection = (req, res, next) => {
     BranchSchema.find()
     .then((response) => {
@@ -25,6 +46,25 @@ export const loadBranchCollection = (req, res, next) => {
         })
     })  
     .catch((error) => {
-        console.log(error)
+        res.status(401).json({
+            success: false,
+            error: error
+        })
+    })
+}
+
+export const deleteById = (req, res, next) => {
+    BranchSchema.findByIdAndDelete(req.params.id)
+    .then(() => {
+        res.status(200).json({
+            success: true,
+            message: "branch deleted successfully"
+        })
+    })
+    .catch((error) => {
+        res.status(401).json({
+            success: false,
+            error: error
+        })
     })
 }
--- backend/routes/branch.js
+++ backend/routes/branch.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createBranch, loadBranchCollection } from '../controllers/branch.js';
+import { createBranch, deleteById, getBranchById, loadBranchCollection } from '../controllers/branch.js';
 
 const router = express.Router();
 
 router.route('/branch/new').post(createBranch);
 router.route('/branch').get(loadBranchCollection);
+router.route('/branch/id/:id').get(getBranchById).delete(deleteById);
 
 export {router as branch};
