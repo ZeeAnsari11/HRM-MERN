5f7c4ed0cb3b7c4bc40e52f6975a3632ae46b45d
extensions_phpstudios <extensionsbitbucket@phpstudios.com>
2023-02-21 19:19:46 +0500

    creating department

---
+++ backend/models/departmentSchema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+//  Schema to Create Department 
+
+const departmentSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    branch: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Branch',
+        required: true,
+    },
+    departmentHead: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+})
+
+export default DepartmentModel = mongoose.model('Department', departmentSchema, 'Department Collection')
