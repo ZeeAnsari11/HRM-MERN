eca64ab6059b7f84ec0c43f9963d941d86fef361
MoizADev <114066668+MoizADev@users.noreply.github.com>
2023-02-21 18:45:32 +0500

    defined branch schema and added validation for fields

---
+++ backend/models/branchSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const branchSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter branch name'],
+        maxlength: [45, 'Branch name is too long']
+    },
+    city: {
+        type: String,
+        required: [true, 'Please enter branch city'],
+        maxlength: [65, 'City name is too long']
+    },
+    country: {
+        type: String,
+        required: [true, 'Please enter branch Country'],
+        maxlength: [65, 'Country name is too long']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter branch description'],
+    },
+    manager: {
+        type : mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export const BranchSchema = mongoose.model('Branch', branchSchema);
