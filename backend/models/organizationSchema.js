c806f0a6c6bdae3d5090d0ce2a881659d5ca27ca
Zeeshan Bashir <zeeshan.bscs231@gmail.com>
2023-02-24 14:04:36 +0500

    Schema fixes

--- backend/models/organizationSchema.js
+++ backend/models/organizationSchema.js
@@ -10,39 +10,17 @@ const organizationSchema = mongoose.Schema({
         maxlength: [100, "Organization Name Can't Exceed 100 Characters"],
         trim: true
     },
-    address: {
-        type: String,
-        required: [true, "Please Address of Organization"],
-        maxlength: [100, "Organization Address Can't Exceed 100 Characters"],
-        trim: true
-    },
     start: {
         type: Date,
-    },
-    end: {
-        type: Date,
+        default : Date.now
     },
     description: {
         type: String,
         maxlength: [200, "Organization description Can't Exceed 200 Characters"],
-    },
-    location: {
-        type: {
-            type:String,
-            default:'point'
-        },
-        coordinates: {
-            type: Array,
-            required:[true, 'Please Enter the Longititude and latitude of the Organization']
-        }
-    },
+    }, 
     logo: {
         type: String,
         required: [true, 'Please Enter the Logo for the Organization']
-    },
-    timeZone:{
-        type: String,
-        required : [true , 'Please Enter the time zone for the Organization']
     },
     createdAt: {
         type: Date,
