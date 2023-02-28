b66a14b016ce7eed91988e828ecd0c8b0b0e8d7d
Usman Ghani <extensionsbitbucket@phpstudios.com>
2023-02-28 15:07:05 +0500

    Added User rountes and controllers and Linked the departments across the users

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 import express  from "express";
 import { organizationRoute } from "./routes/organization.js";
 import { branchRouter } from "./routes/branch.js";
-import department from './routes/department.js'
+import { departmentRoute } from './routes/department.js'
+import { userRoute } from './routes/user.js'
 import { qualificationExperiences } from "./routes/qualificationExperiences.js";
 
 const app = express();
@@ -10,7 +11,8 @@ app.use(express.json());
 
 app.use('/api/v1', organizationRoute)
 app.use('/api/v1', branchRouter);
-app.use('/api/v1', department)
+app.use('/api/v1', departmentRoute);
+app.use('/api/v1', userRoute);
 app.use('/api/v1', qualificationExperiences)
 
 export default app;
---
+++ backend/routes/user.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+    createUser, getAllUsersByOrganizationId, getAllUsersByBranchId, updateUserById,
+    deleteUserById
+} from '../controllers/user.js'
+
+export const userRoute = express.Router();
+
+userRoute.route('/user/new').post(createUser)
+userRoute.route('/user/organization/:id').get(getAllUsersByOrganizationId)
+userRoute.route('/user/branch/:id').get(getAllUsersByBranchId)
+userRoute.route('/user/update/:id').put(updateUserById)
+userRoute.route('/user/delete/:id').delete(deleteUserById)
