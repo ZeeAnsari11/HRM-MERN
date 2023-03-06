81296be9e68766620abdae45881a8de18f5de5c5
Zeeshan Bashir <zeeshan.bscs231@gmail.com>
2023-03-06 17:51:14 +0500

    WIP

--- backend/routes/loan.js
+++ backend/routes/loan.js
@@ -5,3 +5,6 @@ export const loanRoute = express.Router();
 
 
 loanRoute.route('/loan/new').post(createLoan);
+bankRoute.route('/loan/:id').get(getLoanById).delete(deleteLoanById).put(updateLoanById);
+bankRoute.route('/loan/approved/:org').get(getAllApprovedLoansByOrgId);
+bankRoute.route('/loan/non-approved/:org').get(getAllApprovedLoansByOrgId);
