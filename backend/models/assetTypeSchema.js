f019cb5e34909c66702ab52d8e2c47333f1ffae7
MoizADev <114066668+MoizADev@users.noreply.github.com>
2023-03-08 14:21:16 +0500

    schema for asset type added

---
+++ backend/models/assetTypeSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const assetTypeSchema = mongoose.Schema({
+    type: {
+        type: String,
+        required: [true, 'Please Enter the Asset Type You want to Create'],
+        maxlength: [70, 'Length can not exceed 70 characters'],
+        trim: true,
+        validate: /^[a-zA-Z ][a-zA-Z ]+$/
+    },
+    organization: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization',
+        required: [true, "Please Enter the Organization"]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export const AssetTypeModel = mongoose.model('AssetType', assetTypeSchema, 'AssetType Collection');
