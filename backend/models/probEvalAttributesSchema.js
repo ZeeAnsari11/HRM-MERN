6a8ba27598179154345c5392be1ca97adcd38d2d
Zeeshan Bashir <zeeshan.bscs231@gmail.com>
2023-03-16 18:19:58 +0500

    Fixed issue of code review

--- backend/models/probEvalAttributesSchema.js
+++ backend/models/probEvalAttributesSchema.js
@@ -1,26 +1,32 @@
-import mongoose from mongoose;
+import mongoose from "mongoose";
 const probEvalAttributesSchema = mongoose.Schema({
-    attributes : [
-        {
-            name : {
-                type : String,
-                required : true
-            },
-            overAllEval : {
-                type : Number,
-                default : 0
-            },
-            performance : {
-                type : Number,
-                default : 0
-            },
-            probFormId :{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "ProbEvalForm",
-                required: true
-            }
-        }
-    ]
+
+    name: {
+        type: String,
+        required: true,
+        trim : true
+    },
+    overAllEval: {
+        type: Number,
+        default: 0,
+        min  :0,
+        max : 10
+    },
+    performance: {
+        type: Number,
+        default: 0,
+        min  :0,
+        max : 10
+    },
+    probFormId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ProbEvalForm",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 export const ProbEvalAttributesModel = mongoose.model('ProbEvalAttributes', probEvalAttributesSchema, 'ProbEvalAttributes Collection')
