cea279424ebc5383d01136f94a3d279166d36122
Usman Ghani <extensionsbitbucket@phpstudios.com>
2023-03-17 18:01:59 +0500

    Added the evaluation rating schema

---
+++ backend/models/evaluationratingsSchema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const evaluationratingsSchema = mongoose.Schema({
+    ratings: {
+        type: String,
+        required: [true, "Enter Ratings"],
+        trim: true,
+        maxLength: [100, 'Cannot exceeds from 100 characters']
+    },
+    organization: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Organization',
+        required: true
+    },
+    unique_id: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export const EvaluationRatingseModel = mongoose.model('Evaluationratings', evaluationratingsSchema, 'Evaluation Ratings Collection')
