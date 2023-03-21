01203bec036739529dbbb6501ce32b9ace3ba790
Zeeshan Bashir <zeeshan.bscs231@gmail.com>
2023-03-21 15:35:39 +0500

    Created Schema for leave Slabs

---
+++ backend/models/leaveSlabsSchema.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const leaveSlabsSchema = new mongoose.Schema({
+    fromBalance : {
+        type: Number,
+        required: [true, "Please enter a balance"],
+        max : 0
+    },
+    toBalance : {
+        type: Number,
+        required: [true, "Please enter a balance"],
+        min : 1
+    },
+    minDays : {
+        type: Number,
+        required: [true, "Please enter a minimum days"]
+    },
+    maxDays : {
+        type: Number,
+        required: [true, "Please enter a maximum days"]
+    },
+    repeat : {
+        type: String,
+        required: [true, "Please enter a repeat"]
+    },
+    deductionType:{
+        type: String,
+        required: [true, "Please enter a deduction type"]
+    }
+})
+
+export const LeaveSlabsModel = mongoose.model('LeaveSlabs', leaveSlabsSchema, 'LeaveSlabs Collection')
