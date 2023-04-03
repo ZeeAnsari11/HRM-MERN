4ad6aba02e2ec7f9a24bc86d2e928ddf8096a33d
Zeeshan Bashir <zeeshan.bscs231@gmail.com>
2023-04-03 13:11:07 +0500

    WIP

--- backend/controllers/timeSlots.js
+++ backend/controllers/timeSlots.js
@@ -21,63 +21,54 @@ export const createTimeSlot = (req, res, next) => {
 };
 
 export const updateTimeSlotById = (req, res, next) => {
-try {
-    TimeSlotsModel.findById(req.params.id)
-        .then((slot) => {
-            if (!slot) {
-                throw ("Time slot not found");
-            }
-            if (req.body.startTime) {
-                req.body.startTime = new Date(placeHolder + req.body.startTime);
-                console.log("=======req.body.startTime===",req.body.startTime);
-            }
-            if (req.body.endTime) {
-                req.body.endTime = new Date(placeHolder + req.body.endTime);
-            }
-            if (req.body.break?.startTime) {
-                req.body.break.startTime = new Date(placeHolder + req.body.break.startTime);
-            }
-            if (req.body.break?.endTime) {
-                req.body.break.endTime = new Date(placeHolder + req.body.break.endTime);
-            }
-            if (req.body.startTime && req.body.startTime.getTime() === slot.endTime.getTime()) {
-                throw ("New startTime cannot be the same as the current endTime");
-            }
-            if (req.body.endTime && req.body.endTime.getTime() === slot.startTime.getTime()) {
-                throw ("New endTime cannot be the same as the current startTime");
-            }
-            if (req.body.break?.startTime && req.body.break?.startTime.getTime() === slot.break?.endTime.getTime()) {
-                throw ("New startTime for break cannot be the same as the current endTime for break");
-            }
-            if (req.body.break?.endTime && req.body.break?.endTime.getTime() === slot.break?.startTime.getTime()) {
-                throw ("New endTime for break cannot be the same as the current startTime for break");
-            }
-            if (req.body.startTime && req.body.endTime && req.body.startTime.getTime() === req.body.endTime.getTime()) {
-                throw ("startTime and endTime cannot be the same");
-            }
-            if (req.body.break?.startTime && req.body.break?.endTime && req.body.break?.startTime.getTime() === req.body.break?.endTime.getTime()) {
-                throw ("startTime and endTime for Break cannot be the same");
-            }
-            console.log("=req.body======", req.body);
-            TimeSlotsModel.findByIdAndUpdate(req.params.id, req.body, { runValidators: true })
-                .then((response) => {
-                    res.status(200).json({
-                        success: true,
-                        message: "Updated Successfully",
+    try {
+        TimeSlotsModel.findById(req.params.id)
+            .then((slot) => {
+                if (!slot) {
+                    throw ("Time slot not found");
+                }
+                if (req.body.startTime) {
+                    req.body.startTime = new Date(placeHolder + req.body.startTime);
+                }
+                if (req.body.endTime) {
+                    req.body.endTime = new Date(placeHolder + req.body.endTime);
+                }
+                if (req.body.break?.startTime) {
+                    req.body.break.startTime = new Date(placeHolder + req.body.break.startTime);
+                }
+                if (req.body.break?.endTime) {
+                    req.body.break.endTime = new Date(placeHolder + req.body.break.endTime);
+                }
+                if (req.body.startTime && req.body.startTime.getTime() === slot.endTime.getTime()) {
+                    throw ("New startTime cannot be the same as the current endTime");
+                }
+                if (req.body.endTime && req.body.endTime.getTime() === slot.startTime.getTime()) {
+                    throw ("New endTime cannot be the same as the current startTime");
+                }
+                if (req.body.break?.startTime && req.body.break?.startTime.getTime() === slot.break?.endTime.getTime()) {
+                    throw ("New startTime for break cannot be the same as the current endTime for break");
+                }
+                if (req.body.break?.endTime && req.body.break?.endTime.getTime() === slot.break?.startTime.getTime()) {
+                    throw ("New endTime for break cannot be the same as the current startTime for break");
+                }
+                if (req.body.startTime && req.body.endTime && req.body.startTime.getTime() === req.body.endTime.getTime()) {
+                    throw ("startTime and endTime cannot be the same");
+                }
+                if (req.body.break?.startTime && req.body.break?.endTime && req.body.break?.startTime.getTime() === req.body.break?.endTime.getTime()) {
+                    throw ("startTime and endTime for Break cannot be the same");
+                }
+                TimeSlotsModel.findByIdAndUpdate(req.params.id, req.body, { runValidators: true })
+                    .then((response) => {
+                        res.status(200).json({
+                            success: true,
+                            message: "Updated Successfully",
+                        })
                     })
-                })
-                .catch((err) => { 
-    console.log("========err=====1====",err);
-    handleCatch(err, res, 401, "TimeSlot") })
-        })
-        .catch((err) => { 
-    console.log("========err=====2====",err);
-    handleCatch(err, res, 401, "TimeSlot") })
-}
-catch (err) {
-    console.log("========err=========",err);
-    handleCatch(err, res, 401, "Time")
-}
+                    .catch((err) => { handleCatch(err, res, 401, "TimeSlot") })
+            })
+            .catch((err) => { handleCatch(err, res, 401, "TimeSlot") })
+    }
+    catch (err) { handleCatch(err, res, 401, "Time") }
 };
 
 export const getTimeSlotsByOrganizationId = (req, res, next) => {
