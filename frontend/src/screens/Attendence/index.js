218bc196ed031c53fe8dbf932e299438db6d2308
Zeeshan Bashir <zeeshan.bscs231@gmail.com>
2023-06-28 19:56:19 +0500

    Change user creation forms layout

--- frontend/src/screens/Attendence/index.js
+++ frontend/src/screens/Attendence/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentUser } from "../../states/reducers/slices/backend/UserSlice";
 import { saveFormDataForMissingPunches } from "../../api/missingPunchesRequests";
 
